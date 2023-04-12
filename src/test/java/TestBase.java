b41f77e11c8616457511f027c1102642680081d4
Alex Ov <alexov@swisscows.email>
2023-04-12 15:52:11 +0300

    Add custom log attach

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -16,27 +16,38 @@ public class TestBase {
 
     private static String executionMode;
 
+    private static StringBuilder myCustomLog = new StringBuilder();
+
+
     @BeforeAll
     static void setUp() {
         // Для аллюр отчета
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         // Configuration.holdBrowserOpen = true;
+        String message;
 
-        Configuration.pageLoadStrategy = "eager";
+                Configuration.pageLoadStrategy = "eager";
 
         logger.info("");
+        myCustomLog.append("\n");
         // Пример первой обработки параметра
 
         logger.info("Sistem property browserSize: " + System.getProperty("browserSize"));
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-		logger.info("Set Configuration.browserSize browserSize: " + Configuration.browserSize + "\n");
+
+        message = "Set Configuration.browserSize browserSize: " + Configuration.browserSize + "\n";
+		logger.info(message);
+        myCustomLog.append(message);
 
         // Пример второй обработки параметра
 
         logger.info("System property remoteURL: " + System.getProperty("remoteURL"));
         Configuration.remote = System.getProperty("remoteURL", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        logger.info("Set configuration.remote: " + Configuration.remote  + "\n");
+
+        message = "Set configuration.remote: " + Configuration.remote  + "\n";
+        logger.info(message);
+        myCustomLog.append(message);
 
         // Конфигурация без указания в джобе:
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -44,14 +55,19 @@ public class TestBase {
                 "enableVNC", true,
                 "enableVideo", true
         ));
-
         Configuration.browserCapabilities = capabilities;
-        logger.info("Set configuration.browserCapabilities: " + Configuration.browserCapabilities  + "\n");
+
+        message = "Set configuration.browserCapabilities: " + Configuration.browserCapabilities  + "\n";
+        myCustomLog.append(message);
+        logger.info(message);
 
     }
 
     @AfterEach
     void addAttachments() {
+
+        Attach.attachAsText("My custom log", myCustomLog.toString() );
+
 /*        Attach.screenshotAs("Last screen");
         Attach.pageSource();
         Attach.browserConsoleLogs();
